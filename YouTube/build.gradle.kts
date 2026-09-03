4ac0a7e7856e660d3b8c3630b1356b2e0b26f195
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 10:42:11 -0700

    Add Kotlin coroutines dependency
    Added dependencies for Kotlin coroutines.

--- YouTube/build.gradle.kts
+++ YouTube/build.gradle.kts
@@ -4,13 +4,15 @@ cloudstream {
     description = "YouTube provider for Movie-Flick"
     authors = listOf("Movie-Flick")
     status = 1
-
     tvTypes = listOf(
         "Other",
         "Live",
         "TvSeries"
     )
-
     language = "en"
     isCrossPlatform = true
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }
