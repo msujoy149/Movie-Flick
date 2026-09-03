319f384b6f40d7535716a362150acdb7729ef6b4
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 04:23:04 -0700

    Create build.gradle.kts

---
+++ DhakaFTP/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Dhaka FTP"
+    authors = listOf("Movie-Flick")
+
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    language = "bn"
+}
