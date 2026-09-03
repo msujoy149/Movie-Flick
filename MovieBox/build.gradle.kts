bdd70dbecbc13974bee6970be8ff16b3a5b657f9
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 06:38:00 -0700

    Create build.gradle.kts

---
+++ MovieBox/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "MovieBox provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    language = "en"
+    isCrossPlatform = true
+}
