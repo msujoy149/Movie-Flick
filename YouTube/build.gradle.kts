c0ee6ef7138dfc2a2f9b834d9eadf51ffebd805c
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 05:53:35 -0700

    Update build.gradle.kts

--- YouTube/build.gradle.kts
+++ YouTube/build.gradle.kts
@@ -1,1 +1,14 @@
+version = 1
 
+cloudstream {
+    description = "YouTube provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+    tvTypes = listOf(
+        "Other",
+        "Live",
+        "TvSeries"
+    )
+    language = "en"
+    isCrossPlatform = true
+}
