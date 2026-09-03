1e1e7066ba23ec678ef8c6ca64903dba70d501b5
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-03 06:21:52 -0700

    Update DhakaFTPPlugin.kt

--- DhakaFTP/src/main/kotlin/com/movieflick/dhakaftp/DhakaFTPPlugin.kt
+++ DhakaFTP/src/main/kotlin/com/movieflick/dhakaftp/DhakaFTPPlugin.kt
@@ -1,1 +1,13 @@
+package com.movieflick.dhakaftp
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DhakaFTPPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        registerMainAPI(DhakaFTP())
+    }
+}
