c2373f3e06c118e0183336db5c919466fdd1cdcf
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-04 00:51:01 -0700

    Rename package to com.movieflick.khulnaplex

--- KhulnaPlex/src/main/kotlin/com/movieflick/khulnaplex/KhulnaPlexPlugin.kt
+++ KhulnaPlex/src/main/kotlin/com/movieflick/khulnaplex/KhulnaPlexPlugin.kt
@@ -1,13 +1,10 @@
-package com.movieflick.kh
-
-ulnaplex
+package com.movieflick.khulnaplex
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
 class KhulnaPlexPlugin : BasePlugin() {
-
     override fun load() {
         registerMainAPI(KhulnaPlex())
     }
