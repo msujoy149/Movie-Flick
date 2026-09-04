9f58f1e7b1e62b51438a2a5a5854453b7e8fe853
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-04 00:30:41 -0700

    Add KhulnaPlexPlugin class for Cloudstream integration

--- KhulnaPlex/src/main/kotlin/com/movieflick/khulnaplex/KhulnaPlexPlugin.kt
+++ KhulnaPlex/src/main/kotlin/com/movieflick/khulnaplex/KhulnaPlexPlugin.kt
@@ -1,1 +1,14 @@
+package com.movieflick.kh
 
+ulnaplex
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class KhulnaPlexPlugin : BasePlugin() {
+
+    override fun load() {
+        registerMainAPI(KhulnaPlex())
+    }
+}
