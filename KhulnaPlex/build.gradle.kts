b9469c8e54187a9c6d054c3c06cc6caa610d2834
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-04 00:29:35 -0700

    Add cloudstream configuration to build.gradle.kts
    Added cloudstream configuration with metadata and supported TV types.

--- KhulnaPlex/build.gradle.kts
+++ KhulnaPlex/build.gradle.kts
@@ -1,1 +1,16 @@
+version = 1
 
+cloudstream {
+    description = "Khulna Plex provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    language = "bn"
+    isCrossPlatform = true
+}
