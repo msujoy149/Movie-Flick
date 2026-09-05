29d8e7f70ff5adb51c8c5d6ba16524bab9f8a6a6
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 04:02:31 -0700

    Update YouTubeKids.kt

--- YouTubeKids/src/main/kotlin/com/movieflick/youtubekids/YouTubeKids.kt
+++ YouTubeKids/src/main/kotlin/com/movieflick/youtubekids/YouTubeKids.kt
@@ -619,7 +619,7 @@ class YouTubeKids : MainAPI() {
         }
 
         val results =
-            mutableListOf<List<InfoItem>>()
+            mutableListOf<List<StreamInfoItem>>()
 
         for (
             batch in cleanQueries.chunked(6)
