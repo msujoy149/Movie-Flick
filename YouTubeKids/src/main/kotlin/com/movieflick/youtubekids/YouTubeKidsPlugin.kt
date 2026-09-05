97802a89e4553c56cf4e5f5d24e2eb16c4208e4d
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 00:36:24 -0700

    Update YouTubeKidsPlugin.kt

--- YouTubeKids/src/main/kotlin/com/movieflick/youtubekids/YouTubeKidsPlugin.kt
+++ YouTubeKids/src/main/kotlin/com/movieflick/youtubekids/YouTubeKidsPlugin.kt
@@ -1,1 +1,11 @@
+package com.movieflick.youtubekids
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class YouTubeKidsPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(YouTubeKids())
+    }
+}
