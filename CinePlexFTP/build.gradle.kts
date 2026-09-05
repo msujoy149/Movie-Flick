d1f730378eb3ffa3ff7ebc15ef10f0013243a091
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 12:04:36 -0700

    Update build.gradle.kts

--- CinePlexFTP/build.gradle.kts
+++ CinePlexFTP/build.gradle.kts
@@ -1,1 +1,33 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.movieflick.cineplexftp"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+version = 1
+
+cloudstream {
+    description = "Cine Plex FTP provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+    language = "bn"
+    iconUrl = "https://cineplexbd.net/favicon.png?v=2"
+    isCrossPlatform = true
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+}
