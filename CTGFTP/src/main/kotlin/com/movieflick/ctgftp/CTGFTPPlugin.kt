d018a9b77f391218a43a50d54a942c7eee56fb74
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 08:43:48 -0700

    Add package and imports to CTGFTPPlugin.kt

--- CTGFTP/src/main/kotlin/com/movieflick/ctgftp/CTGFTPPlugin.kt
+++ CTGFTP/src/main/kotlin/com/movieflick/ctgftp/CTGFTPPlugin.kt
@@ -1,3 +1,8 @@
+package com.movieflick.ctgftp
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
 @CloudstreamPlugin
 class CTGFTPPlugin : BasePlugin() {
     override fun load() {
