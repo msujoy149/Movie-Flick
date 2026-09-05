71e6b2c70deda54207a4d1878d570b80f7092d9f
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 02:41:40 -0700

    Update build.gradle.kts

--- YouTubeKids/build.gradle.kts
+++ YouTubeKids/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 
 android {
     namespace = "com.movieflick.youtubekids"
-
     compileSdk = 35
 
     defaultConfig {
@@ -13,24 +12,18 @@ android {
     }
 }
 
-version = 1
+version = 2
 
 cloudstream {
     description = "Bengali India YouTube Kids provider for Movie-Flick"
     authors = listOf("Movie-Flick")
     status = 1
-
     tvTypes = listOf(
         "Other",
         "TvSeries"
     )
-
-    // Bengali (India) so the provider is shown with the Indian flag.
     language = "bn-IN"
-
-    // Kids-specific provider icon instead of the default puzzle-piece icon.
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/YouTube_Kids_LogoVector.svg/512px-YouTube_Kids_LogoVector.svg.png"
-
     isCrossPlatform = true
 }
 
