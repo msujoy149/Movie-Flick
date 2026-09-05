6627f93eaf03640c9d3b8f21d12b75a1b5c76398
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 08:43:11 -0700

    Configure Android library and Cloudstream settings
    Added Android library configuration and Cloudstream metadata.

--- CTGFTP/build.gradle.kts
+++ CTGFTP/build.gradle.kts
@@ -1,1 +1,30 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.movieflick.ctgftp"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+version = 1
+
+cloudstream {
+    description = "CTG FTP provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+    // FTP providers use the Bangladesh locale/flag.
+    language = "bn"
+    iconUrl = "https://flagcdn.com/w320/bd.png"
+    isCrossPlatform = true
+}
