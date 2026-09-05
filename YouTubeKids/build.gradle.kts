f8d5741e975dbb77c942a4f3fc32e44475551eea
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-05 00:50:20 -0700

    Add Android library plugin and configuration

--- YouTubeKids/build.gradle.kts
+++ YouTubeKids/build.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.movieflick.youtubekids"
+
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
