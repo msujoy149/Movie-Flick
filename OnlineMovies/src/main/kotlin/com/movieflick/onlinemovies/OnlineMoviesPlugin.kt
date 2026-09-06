7c3acc7ede9cefd8c6908fc01739d6d44ee93132
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-06 10:15:16 -0700

    Add OnlineMoviesPlugin class
    Implement OnlineMoviesPlugin for Cloudstream.

--- OnlineMovies/src/main/kotlin/com/movieflick/onlinemovies/OnlineMoviesPlugin.kt
+++ OnlineMovies/src/main/kotlin/com/movieflick/onlinemovies/OnlineMoviesPlugin.kt
@@ -1,1 +1,14 @@
+package com.movieflick.onlinemovies
 
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class OnlineMoviesPlugin : Plugin() {
+
+    override fun load(context: Context) {
+        // Register the Online Movies provider
+        registerMainAPI(OnlineMovies())
+    }
+}
