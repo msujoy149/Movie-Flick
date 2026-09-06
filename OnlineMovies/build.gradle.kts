e623a0fcad0cd892750e686bb0d4d48932e1b175
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-06 10:13:37 -0700

    Update build.gradle.kts

--- OnlineMovies/build.gradle.kts
+++ OnlineMovies/build.gradle.kts
@@ -1,1 +1,34 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
 
+cloudstream {
+    setRepo(
+        System.getenv("GITHUB_REPOSITORY")
+            ?: "msujoy149/Movie-Flick"
+    )
+}
+
+android {
+    namespace = "com.movieflick.onlinemovies"
+
+    defaultConfig {
+        minSdk = 21
+        compileSdk = 35
+        targetSdk = 35
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+}
