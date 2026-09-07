0dcd7b551a7ebba5018f155205a38c31c4bb3235
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-07 12:39:26 -0700

    Update MojaLossPlugin.kt

--- MojaLoss/src/main/kotlin/com/movieflick/mojaloss/MojaLossPlugin.kt
+++ MojaLoss/src/main/kotlin/com/movieflick/mojaloss/MojaLossPlugin.kt
@@ -1,1 +1,14 @@
+```kotlin
+package com.movieflick.mojaloss
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class MojaLossPlugin : BasePlugin() {
+
+    override fun load() {
+        registerMainAPI(MojaLoss())
+    }
+}
+```
