208e964e9fe160021f398b18155c81be338d15d3
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-07 12:38:29 -0700

    Update build.gradle.kts

--- MojaLoss/build.gradle.kts
+++ MojaLoss/build.gradle.kts
@@ -1,1 +1,39 @@
+```kotlin
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.movieflick.mojaloss"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+version = 1
+
+cloudstream {
+    description = "Moja Loss provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+
+    language = "bn"
+
+    iconUrl = "https://www.mojaloss.stream/favicon.png"
+
+    isCrossPlatform = true
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+}
+```
