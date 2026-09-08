26221a6da456e03de4f6b7e0c24271ba6cbbafad
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-08 09:20:03 -0700

    Implement MovieLinkBDPlugin with main API registration

--- MovieLinkBD/src/main/kotlin/com/movieflick/movielinkbd/MovieLinkBDPlugin.kt
+++ MovieLinkBD/src/main/kotlin/com/movieflick/movielinkbd/MovieLinkBDPlugin.kt
@@ -1,1 +1,9 @@
+package com.movieflick.movielinkbd
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class MovieLinkBDPlugin : BasePlugin() {
+    override fun load() { registerMainAPI(MovieLinkBD()) }
+}
