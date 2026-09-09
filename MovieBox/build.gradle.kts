6cd3ce2e8fdfb631610a34475aef3938b8a92a67
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-08 21:21:24 -0700

    Update build.gradle.kts

--- MovieBox/build.gradle.kts
+++ MovieBox/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 version = 1
 
 cloudstream {
@@ -13,4 +19,30 @@ cloudstream {
 
     language = "en"
     isCrossPlatform = true
+    setRepo(
+        System.getenv("GITHUB_REPOSITORY")
+            ?: "msujoy149/Movie-Flick"
+    )
+}
+
+android {
+    namespace = "com.movieflick.moviebox"
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(
+        "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
+    )
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
 }
