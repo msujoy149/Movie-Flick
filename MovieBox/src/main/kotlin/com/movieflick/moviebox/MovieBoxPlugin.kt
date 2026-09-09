0f393e52e0400fe650fb17f97bcdecbda8e63e1e
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-08 21:22:23 -0700

    Refactor MovieBoxPlugin to extend Plugin class

--- MovieBox/src/main/kotlin/com/movieflick/moviebox/MovieBoxPlugin.kt
+++ MovieBox/src/main/kotlin/com/movieflick/moviebox/MovieBoxPlugin.kt
@@ -1,12 +1,12 @@
 package com.movieflick.moviebox
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class MovieBoxPlugin : BasePlugin() {
-
-    override fun load() {
+class MovieBoxPlugin : Plugin() {
+    override fun load(context: Context) {
         registerMainAPI(MovieBox())
     }
 }
