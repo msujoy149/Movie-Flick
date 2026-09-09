b0c8831aafc30ca446acbfebe3e51502820cc2ca
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-09 08:46:12 -0700

    Update build.gradle.kts

--- MovieBox/build.gradle.kts
+++ MovieBox/build.gradle.kts
@@ -40,9 +40,6 @@ android {
 }
 
 dependencies {
-    implementation(
-        "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
-    )
     implementation(kotlin("stdlib"))
     implementation("com.github.Blatzar:NiceHttp:0.4.11")
     implementation("org.jsoup:jsoup:1.18.3")
