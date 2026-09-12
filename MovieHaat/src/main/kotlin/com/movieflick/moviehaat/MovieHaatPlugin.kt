c7ca8d7cb177d293e2e4a258f2b19865e5852390
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-12 03:20:22 -0700

    Implement MovieHaatPlugin with main API registration

--- MovieHaat/src/main/kotlin/com/movieflick/moviehaat/MovieHaatPlugin.kt
+++ MovieHaat/src/main/kotlin/com/movieflick/moviehaat/MovieHaatPlugin.kt
@@ -1,1 +1,11 @@
+package com.movieflick.moviehaat
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class MovieHaatPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(MovieHaat())
+    }
+}
