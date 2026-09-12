96b253074bc2134bc0130a1e05e97c4d5569529c
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-12 03:19:25 -0700

    Update build.gradle.kts

--- MovieHaat/build.gradle.kts
+++ MovieHaat/build.gradle.kts
@@ -1,1 +1,13 @@
+version = 1
 
+cloudstream {
+    description = "Movie Haat provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+    language = "bn"
+    isCrossPlatform = true
+}
