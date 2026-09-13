6274f1a71a4ea6f85baea01c78f7617da16f828b
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-13 02:30:04 -0700

    Update HiAnimePlugin.kt

--- HiAnime/src/main/kotlin/com/movieflick/hianime/HiAnimePlugin.kt
+++ HiAnime/src/main/kotlin/com/movieflick/hianime/HiAnimePlugin.kt
@@ -1,2 +1,12 @@
-tfdtrdtdrtydrtdtd
-tdtdftf
+package com.movieflick.hianime
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class HiAnimePlugin : BasePlugin() {
+
+    override fun load() {
+        registerMainAPI(HiAnime())
+    }
+}
