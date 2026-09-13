e17be51b95d4e06aa8b4dc251b770a858bd74336
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-12 23:08:56 -0700

    Implement BasPlayFTPPlugin class with load method

--- BasPlayFTP/src/main/kotlin/com/movieflick/basplayftp/BasPlayFTPPlugin.kt
+++ BasPlayFTP/src/main/kotlin/com/movieflick/basplayftp/BasPlayFTPPlugin.kt
@@ -1,1 +1,11 @@
+package com.movieflick.basplayftp
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class BasPlayFTPPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(BasPlayFTP())
+    }
+}
