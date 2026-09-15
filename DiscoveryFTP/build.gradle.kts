cacb8a5d2e1460a4df87d67e7e807482c32409e0
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-15 13:24:09 -0700

    Update build.gradle.kts

--- DiscoveryFTP/build.gradle.kts
+++ DiscoveryFTP/build.gradle.kts
@@ -1,1 +1,37 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.movieflick.discoveryftp"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+version = 1
+
+cloudstream {
+    description = "Discovery FTP provider for Movie-Flick"
+    authors = listOf("Movie-Flick")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+    language = "bn"
+    isCrossPlatform = true
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+}
