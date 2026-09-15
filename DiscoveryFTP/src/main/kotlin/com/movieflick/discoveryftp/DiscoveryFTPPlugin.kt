6f5807edc829e3ed6396cfbb749457a20ccdf28f
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-15 13:24:54 -0700

    Implement DiscoveryFTPPlugin class with load method

--- DiscoveryFTP/src/main/kotlin/com/movieflick/discoveryftp/DiscoveryFTPPlugin.kt
+++ DiscoveryFTP/src/main/kotlin/com/movieflick/discoveryftp/DiscoveryFTPPlugin.kt
@@ -1,1 +1,11 @@
+package com.movieflick.discoveryftp
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class DiscoveryFTPPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(DiscoveryFTP())
+    }
+}
