2b8ee8626b2692e1aee677b638217d0042537cc9
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-18 09:36:31 -0700

    Update ZoryvaPlugin.kt

--- Zoryva/src/main/kotlin/com/zoryva/zoryva/ZoryvaPlugin.kt
+++ Zoryva/src/main/kotlin/com/zoryva/zoryva/ZoryvaPlugin.kt
@@ -1,1 +1,11 @@
+package com.movieflick.zoryva
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class ZoryvaPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Zoryva())
+    }
+}
