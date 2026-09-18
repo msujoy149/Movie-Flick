a77648e9d06a6f9269657bad9a6893d4f138d162
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-18 09:36:03 -0700

    Update build.gradle.kts

--- Zoryva/build.gradle.kts
+++ Zoryva/build.gradle.kts
@@ -1,1 +1,46 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
 
+version = 1
+
+cloudstream {
+    description = "Zoryva CloudStream provider"
+    authors = listOf("Zoryva")
+    status = 1
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime"
+    )
+    language = "en"
+    iconUrl = "https://zoryva.me/icon-512.png"
+    isCrossPlatform = true
+    setRepo(
+        System.getenv("GITHUB_REPOSITORY")
+            ?: "msujoy149/Movie-Flick"
+    )
+}
+
+android {
+    namespace = "com.movieflick.zoryva"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+}
