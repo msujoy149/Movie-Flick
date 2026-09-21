52a1d14156843eb371d4f2dc97fada6972889b10
msujoy149 <mondalsujoy1147@gmail.com>
2026-09-21 10:57:57 -0700

    Refactor MovieBoxPlugin to remove context parameter
    Updated MovieBoxPlugin to register MovieBox without context.

--- MovieBox/src/main/kotlin/com/movieflick/moviebox/MovieBoxPlugin.kt
+++ MovieBox/src/main/kotlin/com/movieflick/moviebox/MovieBoxPlugin.kt
@@ -4,9 +4,15 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
+/**
+ * Build-time placeholder only.
+ *
+ * The workflow replaces the generated MovieBox.cs3 with the
+ * prebuilt, proven MovieBoxProvider artifact before publishing.
+ */
 @CloudstreamPlugin
 class MovieBoxPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(MovieBox(context))
+        registerMainAPI(MovieBox())
     }
 }
